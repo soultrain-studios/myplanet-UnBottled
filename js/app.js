7e3e20370a3d7121cc4a9310cd9a115426e848fb
mtamer <mark.tamer01@gmail.com>
2014-05-09 14:51:31 -0400

    Page reads from firebase database

--- js/app.js
+++ js/app.js
@@ -1,28 +1,18 @@
-angular.module("unbottled", [])
-  .factory("Water", function() {
-    var ref = new Firebase("https://unbottled.firebaseio.com/Organizations");
-    return {
-      getMessages: function() {
-        var messages = [];
-        ref.on("child_added", function(snapshot) {
-          messages.push(snapshot.val());
-        });
-        return messages;
-      },
-      addMessage: function(message) {
-        ref.push(message);
-      }
-    }
-  })
-  .controller("UnbottledController", ["$scope", "Water",
-    function($scope, service) {
-      $scope.user = "Guest " + Math.round(Math.random()*101);
-      $scope.messages = service.getMessages();
-      $scope.addMessage = function() {
-        service.addMessage({from: $scope.user, content: $scope.message});
-        $scope.message = "";
-      };
+angular.module("unbottled", ["firebase"])
+  .factory("WaterInfo", ["$firebase", function($firebase) {
+    var ref = new Firebase("https://unbottled.firebaseio.com");
+    return $firebase(ref);
+     }])
+  .factory("OrgInfo", ["$firebase", function($firebase) {
+    var ref = new Firebase("https://unbottled.firebaseio.com/orgInfo");
+    return $firebase(ref);
+     }])
+  .controller ("UnbottledController", ["$scope", "WaterInfo", "OrgInfo",
+    function($scope, WaterInfo, OrgInfo) {
+      // Get types & ratings.
+      $scope.WaterInfos = WaterInfo;
+      $scope.OrgInfos = OrgInfo;
+        // Add it via $add on the service. 
     }
   ]);
 
-  
