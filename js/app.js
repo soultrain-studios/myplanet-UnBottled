4ca0bbeb7f94694484dde556b347e694051c534c
mtamer <mark.tamer01@gmail.com>
2014-05-09 16:53:31 -0400

    Firebase appending properly

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
 angular.module("unbottled", ["firebase"])
   .factory("WaterInfo", ["$firebase", function($firebase) {
-    var ref = new Firebase("https://unbottled.firebaseio.com");
+    var ref = new Firebase("https://unbottled1.firebaseio.com/");
     return $firebase(ref);
      }])
-  .factory("OrgInfo", ["$firebase", function($firebase) {
-    var ref = new Firebase("https://unbottled.firebaseio.com/orgInfo");
-    return $firebase(ref);
-     }])
-  .controller ("UnbottledController", ["$scope", "WaterInfo", "OrgInfo",
-    function($scope, WaterInfo, OrgInfo) {
+  .controller ("UnbottledController", ["$scope", "WaterInfo",
+    function($scope, WaterInfo) {
       // Get types & ratings.
       $scope.WaterInfos = WaterInfo;
-      $scope.OrgInfos = OrgInfo;
         // Add it via $add on the service. 
     }
   ]);
