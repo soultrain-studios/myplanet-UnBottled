9cb3161c9d6a0b96e2b00886bccde5d5d8ba4dda
anujdalal <anujd744@gmail.com>
2014-05-10 16:24:12 -0400

    added carousel

--- index.php
+++ index.php
@@ -2,10 +2,52 @@
 
 <!-- CONTENT AREA -->
 
-  <div id="home-about">
+  <!--div id="home-about">
     <img src="images/water04.jpg">
     <p>1.75 billion people in this world do not have access to clean water. This is our solution!</p>
+  </div-->
+
+
+  <!--Background-->
+
+
+<!-- Carousel -->
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="top: 0px; margin-bottom: 100px"> 
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+  
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+  
+    <div class="item active" style="height: 400px; background: url(images/carousel/slide01.jpg) bottom center;">
+      <div class="carousel-caption">
+        <h1>1.75 billion people in this world do not have access to clean water. This is our solution!</h1>
+      </div>
+    </div>
+    
+    <div class="item" style="height: 400px; background: url(images/carousel/slide02.jpg) bottom center;"> 
+      <div class="carousel-caption">
+        <h1>The Water Project</h1>
+        <p><button class="btn btn-success">Learn More</button></p>
+      </div>
+    </div>
+    
+    <div class="item" style="height: 400px; background: url(images/carousel/slide03.jpg) bottom center;"> 
+      <div class="carousel-caption">
+        <h1>Donate to a good cause, volunteer!</h1>
+        <p><button class="btn btn-success">Sign Up</button></p>
+      </div>
+    </div>
+    
   </div>
+  
+  <!-- Controls --> 
+  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left"></span> </a> <a class="right carousel-control" href="#carousel-example-generic" data-slide="next"> <span class="glyphicon glyphicon-chevron-right"></span> </a> </div>
+<!-- div class="main container" --> 
 
 
 <?php include('footer.php'); ?>
