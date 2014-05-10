6a855f5bc6ce3152b016d02e49caee04677e3bcb
Nan <nancy@nangoo.org>
2014-05-10 17:10:12 -0400

    About page

--- about.php
+++ about.php
@@ -1,12 +1,18 @@
 <?php include('header.php'); ?>
 
 <!-- CONTENT AREA -->
+	
+	<div style="text-align: center">
+	<h1>Who are we? What is this place?</h1>
+	<p style="font-size: 16pt">Unbottled brings the most relevent Non-Profit Organizations (NPOs) together in one place, and connects them to the rest of the world! </p>
 
-  <h4><strong>Who are we? What is this place?</strong></h4>
-  <p class="about">Unbottled brings the most relevent Non-Profit Organizations (NPOs) together in one place, and connects them to the rest of the world! </p>
-  <h4><strong>Our goal:</strong></h4>
-  <p>To help NPO's provide clean water to people that don't have it. We do this by connecting volunteers to various NPOs that actively work towards solving water scarcity, and have the right tools and resources to do it.<p>
-  
+
+	<img src="images/team.JPG" width="80%" style="text-align: center" /><br /><br />
+
+
+	<h1>Our goal</h1>
+	<p style="font-size: 16pt">To help NPO's provide clean water to people that don't have it. We do this by connecting volunteers to various NPOs that actively work towards solving water scarcity, and have the right tools and resources to do it.</p>
+	</div>
 
 
 <?php include('footer.php'); ?>
