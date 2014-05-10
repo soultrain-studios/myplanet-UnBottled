d64a1e5afbfa3b24b37f595e3fddd9bfb7554285
anujdalal <anujd744@gmail.com>
2014-05-10 16:59:00 -0400

    added some info

--- about.php
+++ about.php
@@ -2,8 +2,11 @@
 
 <!-- CONTENT AREA -->
 
-  <p class="about">About page!</p>
-
+  <h4><strong>Who are we? What is this place?</strong></h4>
+  <p class="about">Unbottled brings the most relevent Non-Profit Organizations (NPOs) together in one place, and connects them to the rest of the world! </p>
+  <h4><strong>Our goal:</strong></h4>
+  <p>To help NPO's provide clean water to people that don't have it. We do this by connecting volunteers to various NPOs that actively work towards solving water scarcity, and have the right tools and resources to do it.<p>
+  
 
 
 <?php include('footer.php'); ?>
